67fabab1996251e45f3da8aaf362fd5457ee967f
bportela <bportela.dev@gmail.com>
2022-12-24 11:05:28 +0100

    chorradas

--- samples/psc-blazor-test/PscBlazorTest/PscBlazorTest/node-webpack/src/index.js
+++ samples/psc-blazor-test/PscBlazorTest/PscBlazorTest/node-webpack/src/index.js
@@ -1,5 +1,12 @@
 import { getCurrentTime } from "./time-lib.js";
+import { Chart} from 'chart.js'
 
 function BlazorGetTime() {
     return getCurrentTime();
+}
+
+let chart = new Chart();
+
+export {
+    BlazorGetTime
 }
